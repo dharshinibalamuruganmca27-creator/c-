a06a06278127e8b3ef53737f2a6da2fd797d6ea6
dharshinibalamuruganmca27-creator <dharshinibalamurugan_mca27@mepcoeng.ac.in>
2026-04-28 14:17:17 +0530

    findingthepercnetage.py

---
+++ findingthepercnetage.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+    # Extract the list of scores for the specific student
+    scores = student_marks[query_name]
+    
+    # Calculate the average
+    average = sum(scores) / len(scores)
+    
+    # Print formatted to 2 decimal places
+    print("{:.2f}".format(average))
